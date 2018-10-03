7bd883e7db320e5557291c88712f8c7fd75b31af
Ipsita Pathak <ipsitap2@illinois.edu>
2018-10-02 20:45:42 -0500

    i hate myself

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -20,13 +20,118 @@ public class BankAccount {
         WORKPLACE
     }
 
+    /**
+     *
+     */
     private int accountNumber;
-    public BankAccountType accountType;
-    private double accountBalance;
-    private String ownerName;
-    public double interestRate;
-    private double interestEarned;
+    /**
+     * setter
+     * @param setAcc iudhiwuhd
+     */
 
+    public void setAccountNumber(final int setAcc) {
+        this.accountNumber = setAcc;
+    }
+
+    /**
+     * getter
+     * @return
+     */
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+    /**
+     *
+     */
+    private BankAccountType accountType;
+    /**
+     *
+     */
+    private double accountBalance;
+    /**
+     * setter
+     * @param setBalance jfk
+     */
+
+    public void setAccountBalanceBalance(final double setBalance) {
+        this.accountBalance = setBalance;
+    }
+
+    /**
+     * getter
+     * @return
+     */
+    public double getAccountBalance() {
+        return this.accountNumber;
+    }
+    /**
+     *
+     */
+
+    private String ownerName;
+    /**
+     * setter
+     * @param setownerName jfk
+     */
+
+    public void setOwnerName(final String setownerName) {
+        this.ownerName = setownerName;
+    }
+
+    /**
+     * getter
+     * @return ownername
+     */
+    public String getOwnerName() {
+        return this.ownerName;
+    }
+    /**
+     *
+     */
+
+    private double interestRate;
+    /**
+     * setter
+     * @param setinterestRate jfk
+     */
+
+    public void setInterestRate(final double setinterestRate) {
+        this.interestRate = setinterestRate;
+    }
+
+    /**
+     * getter
+     * @return interest rate
+     */
+    public double getInterestRate() {
+        return this.interestRate;
+    }
+    /**
+     *
+     */
+    private double interestEarned;
+    /**
+     * setter
+     * @param setinterestEarned jfk
+     */
+
+    public void setInterestEarned(final double setinterestEarned) {
+        this.interestEarned = setinterestEarned;
+    }
+
+    /**
+     * getter
+     * @return interest rate
+     */
+    public double getInterestEarned() {
+        return this.interestEarned;
+    }
+
+    /**
+     *
+     * @param name fgb
+     * @param accountCategory dvfvdf
+     */
     public BankAccount(final String name, final BankAccountType accountCategory) {
         /*
          * Implement this function
